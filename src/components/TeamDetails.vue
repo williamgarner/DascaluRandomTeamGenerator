e66fd94c1294f5bcd6c575f09b02d6a1b71982fa
nmarno68 <narnold@nevada.unr.edu>
2019-12-07 14:48:05 -0800

    full page team thing kinda alright

--- src/components/TeamDetails.vue
+++ src/components/TeamDetails.vue
@@ -3,18 +3,21 @@
         <v-btn class="button fullscreen" color="blue darken-4 white--text" @click="dialog=true" dark tile>FULL SCREEN</v-btn>
         <v-dialog v-model="dialog" :fullscreen="fullScreen" transition="dialog-bottom-transition" :overlay=false
                   scrollable class="noPaddingPlz">
-            <v-card>
+            <v-card class="classDisplay">
                 <v-toolbar style="flex: 0 0 auto;" class="tool-bar" >
+                    <div class="toTheLeft">Generated Teams</div>
                     <v-btn color="blue darken-4 white--text" class="button close" @click.native="dialog = false" dark fab>
                         x
                     </v-btn>
-
                 </v-toolbar>
-                <v-card-text class="classDisplay">
-                    <v-card-title>
-                        Nutrition
-                        <v-spacer></v-spacer>
-                    </v-card-title>
+                <v-card-text>
+                    <div class="card-control">
+                        <div class="card" v-for="team in teams">
+                            <v-card tile min-height="20rem" min-width="15rem">
+                                {{team.number}}
+                            </v-card>
+                        </div>
+                    </div>
                 </v-card-text>
             </v-card>
         </v-dialog>
@@ -30,6 +33,28 @@
             search: '',
             pagination: {},
             selected: [],
+            teams: [
+                {number: 'one'},
+                {number:'two'},
+                {number:'three'},
+                {number:'four'},
+                {number:'five'},
+                {number: 'one'},
+                {number:'two'},
+                {number:'three'},
+                {number:'four'},
+                {number:'five'},
+                {number: 'one'},
+                {number:'two'},
+                {number:'three'},
+                {number:'four'},
+                {number:'five'},
+                {number: 'one'},
+                {number:'two'},
+                {number:'three'},
+                {number:'four'},
+                {number:'five'},
+            ]
 
         }),
         computed: {
@@ -47,27 +72,44 @@
     .noPaddingPlz{
         padding: 0;
     }
+    .card{
+        margin: 1rem;
+    }
     .classDisplay{
         background-image: url("../../assets/images/betterBlue.jpg");
         background-attachment: fixed;
         background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
+
+        color: white;
+
     }
     .tool-bar{
         background-color: #202020;
-        justify-content: flex-end;
         display: flex;
         padding: 0 1rem;
+        color: white;
+        justify-content: flex-start;
+        width: 100%;
+
     }
     #toolTitle{
         flex-grow: 10;
         color: white;
     }
+    .card-control{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+
+    }
     .close{
         font-size: 1.5rem;
         max-width: 2rem;
         max-height: 2rem;
+        justify-self: flex-end;
+        margin-left: 94rem;
     }
     .fullscreen{
         font-size: 1.25rem;
@@ -77,4 +119,9 @@
         flex-grow: 1;
         font-style: normal;
     }
+    .toTheLeft{
+        justify-self: flex-start;
+    }
+
+
 </style>
